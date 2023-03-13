2749f6c2a8ad878f3b41b7b07eb901bd05da5bfc
aurelien-gallea <aurelien.gallea@laplateforme.io>
2023-03-13 12:27:28 +0100

    ajout des job 01 à 07

---
+++ job02/main.c
@@ -0,0 +1,7 @@
+#include <stdio.h>
+
+int main() {
+    char msg = 'X';
+    putchar(msg);
+    return 0;
+}
---
+++ job07/main.c
@@ -0,0 +1,21 @@
+# include <stdio.h>
+
+
+int main() {
+
+    float a = 0;
+    float b = 0;
+    printf("veuillez rentrer un nombre : ");
+    scanf("%f",&a);
+    printf("veuillez rentrer un autre nombre : ");
+    scanf("%f",&b);
+    
+
+
+    printf("%.2f \n", a/b);
+    printf("%.2f \n", a + b);
+    printf("%.2f \n", a - b);
+    printf("%.2f \n", a * b);
+    return 0;
+    
+}
